d139d50e89d524411329e18d3ac5b693884a772f
Alessandro <alessandro.maric@studenti.unicam.it>
2023-01-10 12:36:20 +0100

    Refactor classi e metodi da italiano a inglese, aggiunta interfaccia FidelityProgram e metodi 2

---
+++ Backend/src/main/java/it/unicam/cs/ids/loyaltyPlatform/model/PointsFidelityProgram.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.loyaltyPlatform.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class PointsFidelityProgram {
+    private @NonNull String nome;
+    private @NonNull int id;
+}
