d04993ae4da21f20f5ea3df7c4f28630791dd557
KLEA PICI <klea.pici@studenti.unicam.it>
2023-01-10 18:10:03 +0100

    Aggiunto altre classi

---
+++ Backend/src/main/java/it/unicam/cs/ids/loyaltyPlatform/controller/UserController.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.ids.loyaltyPlatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class UserController {
+}
