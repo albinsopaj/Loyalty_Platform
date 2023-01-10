4de9d83f9ada60ac7f485b0d85da97ceb297285f
Alessandro <alessandro.maric@studenti.unicam.it>
2023-01-10 12:36:20 +0100

    Refactor classi e metodi da italiano a inglese, aggiunta interfaccia FidelityProgram e metodi

--- Backend/src/main/java/it/unicam/cs/ids/loyaltyPlatform/model/FidelityProgram.java
+++ Backend/src/main/java/it/unicam/cs/ids/loyaltyPlatform/model/FidelityProgram.java
@@ -1,15 +1,44 @@
 package it.unicam.cs.ids.loyaltyPlatform.model;
 
-import lombok.*;
+/**
+    Interface that defines fidelity programs
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class FidelityProgram {
-    private @NonNull String nome;
-    private @NonNull int id;
-    private @NonNull Tipologia tipologia;
+ */
+public interface FidelityProgram {
 
+    /**
+     * Creates a fidelity program given name and description, the program will not be activated until personalized
+     *
+     * @param name of the fidelity program
+     * @param description of the fidelity program
+     */
+    void createFidelityProgram(String name, String description);
+
+    /**
+     * Personalizes a fidelity program
+     *
+     * @param name of the fidelity program to personalize
+     */
+    void personalizeFidelityProgram(String name);
+
+    /**
+     * Activates the fidelity program
+     *
+     * @param name of the fidelity program to activate
+     */
+    void activateFidelityProgram(String name);
+
+    /**
+     * Deactivates the fidelity program
+     *
+     * @param name of the fidelity program to deactivate
+     */
+    void deactivateFidelityProgram(String name);
+
+    /**
+     * Resets a fidelity program, this means that every client's progress using that program will be resetted
+     *
+     * @param name of the fidelity program to reset
+     */
+    void resetFidelityProgram(String name);
 }
