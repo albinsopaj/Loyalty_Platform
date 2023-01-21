bcb48dc01f2b5518322582a2f5ff9fe836088661
KLEA PICI <klea.pici@studenti.unicam.it>
2023-01-21 15:58:23 +0100

    Implementazione classe Promozioni

---
+++ Backend/src/main/java/it/unicam/cs/ids/loyaltyPlatform/model/campaign/Promozioni.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.ids.loyaltyPlatform.model.campaign;
+
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Promozioni {
+
+    private @NonNull final int  ultimiArrivi;
+    private @NonNull final int sconti;
+
+    public Promozioni(@NonNull int ultimiArrivi,@NonNull int sconti){
+        this.ultimiArrivi=ultimiArrivi;
+        this.sconti=sconti;
+    }
+
+    }
+
+
+
+
+
